a35fd1806344871579c0bbb7806732e5d1acc59d
Matt Menzenski <matt.menzenski@gmail.com>
2015-12-16 20:21:04 -0600

    commit

--- cards/models.py
+++ cards/models.py
@@ -6,12 +6,12 @@ class Recipient(models.Model):
     last_name = models.CharField(max_length=100)
     family_relationship = models.CharField(
             max_length=200, default="#davethecat's third-favorite",
-            blank=True, null=True)
-    personalized_greeting = models.CharField(
-            max_length=1000, blank=True, null=True)
+            blank=True)
+    personalized_greeting = models.TextField(default="Merry Christmas!")
 
     class Meta:
         verbose_name_plural = "recipients"
 
     def __unicode__(self):
         return self.first_name
+
--- yearendsite/urls.py
+++ yearendsite/urls.py
@@ -17,5 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^cards/', include('cards.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
